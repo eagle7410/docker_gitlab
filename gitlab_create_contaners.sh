bd7e00cf15f1f0eec44696cf002817ee8e4d5654
Igor <verycooleagle@gmail.com>
2018-07-11 21:36:18 +0300

    More detail in create contaner.

--- gitlab_create_contaners.sh
+++ gitlab_create_contaners.sh
@@ -2,7 +2,7 @@
 
 docker run --detach \
     --hostname gitlab.eagle.com \
-    --publish 443:443 --publish 8084:80 --publish 22:22 \
+    --publish 0.0.0.0:443:443 --publish 0.0.0.0:8084:80 --publish 0.0.0.0:22:22 \
     --name gitlab \
     --restart always \
     --volume /srv/gitlab/config:/etc/gitlab \
